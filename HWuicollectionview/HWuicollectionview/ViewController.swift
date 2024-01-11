222b70d1ae5a1792e95fdad0850928afc10687bf
Karrie <karinad000@gmail.com>
2024-01-11 22:37:51 +0300

    Update ViewController.swift

--- HWuicollectionview/HWuicollectionview/ViewController.swift
+++ HWuicollectionview/HWuicollectionview/ViewController.swift
@@ -11,13 +11,13 @@ import Foundation
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
 //    let watchSFSymbol: UIImageView = UIImageView(image: UIImage(systemName: SFSymbols.watch)?.withRenderingMode(.alwaysTemplate))
 //    let airpodsSFSymbol: UIImageView = UIImageView(image: UIImage(systemName: SFSymbols.airpods)?.withRenderingMode(.alwaysTemplate))
 //    let mouseSFSymbol: UIImageView = UIImageView(image: UIImage(systemName: SFSymbols.mouse)?.withRenderingMode(.alwaysTemplate))
 //    let pencilSFSymbol: UIImageView = UIImageView(image: UIImage(systemName: SFSymbols.pencil)?.withRenderingMode(.alwaysTemplate))
 //    let ipadSFSymbol: UIImageView = UIImageView(image: UIImage(systemName: SFSymbols.ipad)?.withRenderingMode(.alwaysTemplate))
 
-//    let image: [String] = []
     let image = ["applewatch.side.right",
                  "airpods.gen3",
                  "magicmouse",
@@ -28,11 +28,10 @@ class ViewController: UIViewController {
                  "macpro.gen3.fill"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let image = [Image(image1: UIImageView(image: UIImage(systemName: SFSymbols.watch)?.withRenderingMode(.alwaysTemplate)), image2: UIImageView(image: UIImage(systemName: SFSymbols.airpods)?.withRenderingMode(.alwaysTemplate)), image3:  UIImageView(image: UIImage(systemName: SFSymbols.mouse)?.withRenderingMode(.alwaysTemplate)), image4: UIImageView(image: UIImage(systemName: SFSymbols.pencil)?.withRenderingMode(.alwaysTemplate)), image5: UIImageView(image: UIImage(systemName: SFSymbols.ipad)?.withRenderingMode(.alwaysTemplate)))]
-//
     
         setupCollectionView()
+        
+        //добавление вручную
 //        setupWatchSFSymbol()
 //        setupAirpodsSFSymbol()
 //        setupMouseSFSymbol()
@@ -53,17 +52,20 @@ class ViewController: UIViewController {
     }
     
 }
-    extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return image.count
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
-            cell.configure(with: image[indexPath.item])
-         //   let images = image[indexPath.row]
-            return cell
-        }
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return image.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
+        cell.configure(with: image[indexPath.item])
+        //   let images = image[indexPath.row]
+        return cell
+    }
+    
+}
+        //добавление вручную
 //        private func setupWatchSFSymbol() {
 //            watchSFSymbol.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
 //            collectionView.addSubview(watchSFSymbol)
@@ -83,7 +85,7 @@ class ViewController: UIViewController {
 //            pencilSFSymbol.frame = CGRect(x: 300, y: 0, width: 100, height: 100)
 //            collectionView.addSubview(pencilSFSymbol)
 
-        }
+     //   }
     
     
 
